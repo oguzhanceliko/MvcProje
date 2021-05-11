56c4315b1f7e4dc4cf74b64300bfb10c6ebe551e
Oguzhan Celik <ogzzcelik@gmail.com>
2021-05-11 16:35:12 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public  class Contact
+    {
+        [Key]
+        public int ContactId { get; set; }
+        [StringLength(50)]
+        public string ContactUserName { get; set; }
+        [StringLength(50)]
+        public string ContactMail { get; set; }
+        [StringLength(50)]
+        public string ContactSubject { get; set; }
+        
+        public string ContactMessage { get; set; }
+
+        //Icollection yazdığımız yeri orada yazılan collection tablosunda gösteririz.
+
+    }
+}
